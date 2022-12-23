4f7cc5009fc627616134f9551926d777214816ab
ashwinidesale2002 <121277563+ashwinidesale2002@users.noreply.github.com>
2022-12-23 15:47:15 +0530

    Add files via upload

---
+++ Printable.java
@@ -0,0 +1,51 @@
+package StringEx1;
+
+	interface Printable1
+	{
+		void show();
+	}
+	interface Calculate
+	{
+		int calcu(int i);
+	}
+	class Cal_square implements Printable1,Calculate
+	{
+		public void show()
+		{
+			System.out.println("Square of a Number is =");
+		}
+		
+		public int calcu(int a)
+		{
+			return a*a;
+		}
+		
+	}
+	class Cal_cube implements Printable1,Calculate
+	{
+		public void show()
+		{
+			System.out.println("Cube of a Number is=");
+		}
+		public int calcu(int a)
+		{
+			return a*a*a;
+		}
+	}
+
+public class Printable {
+
+		public static void main(String[] args) {
+			
+			Cal_square cq=new Cal_square();
+			cq.show();
+			System.out.println(cq.calcu(2));
+			Cal_cube cc=new Cal_cube();
+			cc.show();
+			System.out.println(cc.calcu(3));	
+			
+		}
+
+	}
+
+
---
+++ String11.java
@@ -0,0 +1,34 @@
+package EqualString1;
+
+import java.util.Scanner;
+
+public class String11 {
+		public static void main(String[] args)
+		{
+			Scanner sc=new Scanner(System.in);
+			
+				System.out.println("Enter first string");
+				System.out.println("Enter second string");
+				String f=sc.nextLine();
+				String s=sc.nextLine();
+				if(f.equals(s))
+				{
+
+					System.out.println("both string are same");
+					
+				}
+				else
+				{
+					System.out.println("both string are not same");
+					
+				}
+				
+				
+			}
+		
+	
+
+	}
+
+
+
